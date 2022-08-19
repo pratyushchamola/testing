0d981ddbc5173b2c0e78acd9d5a9f041561cc5d7
pratyushchamola <pratyushchamola@gmail.com>
2022-08-19 18:10:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is for testing purpos , kindly ignore, else if you want to contribute, feel free, hehe</h1>
+    </div>
+  );
+}
+
+export default App;
